e3d0286b915046c051214918f3f4b0d0d1af966e
Shubham-Rawat9 <shubhamrawat97614@gmail.com>
2025-10-21 13:34:39 +0530

    weather app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h3 className='text-2xl text-red-500 text-center'>Weather Application</h3>
+    </>
+  )
+}
+
+export default App
